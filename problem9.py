6bc40aed774a2f3af714af6893911ce8e0b8f8a2
Harish Saud <saudharish770@gmail.com>
2025-05-18 20:58:58 +0545

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -1,4 +1,4 @@
-# 6. Write a python program to calculate the square of a number entered by the user.
+# 6. Write a Python Program to Calculate the square of a number entered by the User.
 a=int(input("Enter a number to square: "))
 print("Squared number is of",a,"is:", a**2)
 # a^2 This is Incorrect.
